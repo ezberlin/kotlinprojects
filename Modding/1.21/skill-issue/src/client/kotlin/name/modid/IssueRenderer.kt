db09115b84138de2b2af4cceb5d7fa91d57cd43e
ezberlin <60936497+ezberlin@users.noreply.github.com>
2024-07-05 17:34:40 +0200

    added more issues

--- Modding/1.21/skill-issue/src/client/kotlin/name/modid/IssueRenderer.kt
+++ Modding/1.21/skill-issue/src/client/kotlin/name/modid/IssueRenderer.kt
@@ -3,9 +3,10 @@ package name.modid
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawContext
 
+// Update the IssueRenderer to store the highest priority issue directly
 class IssueRenderer {
     companion object {
-        var highestPriorityIssueIndex: Int? = null
+        var highestPriorityIssue: Issue? = null
 
         fun renderOverlay(drawContext: DrawContext, issue: Issue) {
             val client = MinecraftClient.getInstance()
--- Modding/1.21/skill-issue/src/client/kotlin/name/modid/SkillIssueClient.kt
+++ Modding/1.21/skill-issue/src/client/kotlin/name/modid/SkillIssueClient.kt
@@ -1,10 +1,10 @@
 package name.modid
 
-import name.modid.IssueRenderer.Companion.highestPriorityIssueIndex
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.network.ClientPlayerEntity
+import net.minecraft.component.DataComponentTypes
 import net.minecraft.item.Items
 
 class SkillIssueClient : ClientModInitializer {
@@ -15,8 +15,31 @@ class SkillIssueClient : ClientModInitializer {
 			15
 		)
 
+		val lowHealth = Issue(
+			{ p: ClientPlayerEntity -> p.health <= 8 },
 
-		val existingIssues = arrayOf(noTotemEquipped)
+			0x60820C01,
+			20
+		)
+
+		val lowFood = Issue(
+			{ p: ClientPlayerEntity -> p.hungerManager.foodLevel <= 8 },
+
+			0x60492100,
+			10
+		)
+
+		val noFood = Issue(
+			{ p: ClientPlayerEntity ->
+				var food = false
+				for (i in 0..8) if (p.inventory.getStack(i).get(DataComponentTypes.FOOD) != null) food = true
+				food},
+
+			0x60492100,
+			12
+		)
+
+		val existingIssues = arrayOf(noTotemEquipped, lowHealth, lowFood, noFood)
 		val presentIssues = mutableListOf<Issue>()
 
 		ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client ->
@@ -25,15 +48,13 @@ class SkillIssueClient : ClientModInitializer {
 				existingIssues.filter { it.issue(player) }
 					.also { filteredIssues -> presentIssues.addAll(filteredIssues) }
 
-				highestPriorityIssueIndex = presentIssues.maxByOrNull { it.priority }?.let { issue ->
-					existingIssues.indexOf(issue)
-				}
+				IssueRenderer.highestPriorityIssue = presentIssues.maxByOrNull { it.priority }
 			}
 		})
 
 		HudRenderCallback.EVENT.register(HudRenderCallback { matrixStack, _ ->
-			if (highestPriorityIssueIndex != null && presentIssues.isNotEmpty()) {
-				IssueRenderer.renderOverlay(matrixStack, presentIssues[highestPriorityIssueIndex!!])
+			IssueRenderer.highestPriorityIssue?.let { issue ->
+				IssueRenderer.renderOverlay(matrixStack, issue)
 			}
 		})
 	}
